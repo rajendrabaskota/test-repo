d895b0cbe6b0a6859eb55559674d1e54a9ec5e01
Rajendra Baskota <rajendra.baskota@fusemachines.com>
2024-06-22 17:36:59 +0545

    third commit feature

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def do_something(parameter):
-    print(f"Parameter: {parameter}")
+def do_something(name, roll_no, grade):
+    print(f"Name: {name}, Roll No. {roll_no} and Class: {grade}")
 
 
 if __name__ == "__main__":
-    parameter = "rajendra baskota is a very handsome guy....."
-    do_something(parameter=parameter)
+    name = "rajendra baskota"
+    roll_no = 21
+    grade = 10
+    do_something(name, roll_no, grade)
